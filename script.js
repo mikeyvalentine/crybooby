446e77b5453b22eeafe90103924e95c09d69fab9
dzychain <michaelsmailmachine@gmail.com>
2020-11-22 23:04:52 -0800

    ad

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 const radio = document.getElementById('rad');
 const menu = document.getElementById('menu');
-
-radio.addEventListener('click', function(){
-    if(menu.style.visibility == 'hidden') {
-        menu.style.visibility = 'visible';
-    }
-    else{
-        menu.style.visibility = 'hidden';
-    }
-})
