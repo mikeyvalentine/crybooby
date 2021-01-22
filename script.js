bc3bb96c3513c3088994fe8bc349c85c5481676c
Michael <michaelsmailmachine@gmail.com>
2021-01-21 17:01:49 -0800

    faa

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const names = ['tott', 'totey', 'toet', 'toee'];
 const srcs = ['./images/tote.png','./images/tote.png','./images/tote.png','./images/tote.png'];;
 class Item {
-    constructor(name, img, a){
+    constructor(name, div, img, a){
         this.name = name;
+        this.div = div;
         this.img = img;
         this.a = a;
     }
@@ -11,24 +12,28 @@ var items = [];
 var menu = document.getElementById('menu');
 function intializeImgs(){
     for(i=0; i<names.length; i++){
-        items.push(new Item(names[i], document.createElement('img'), document.createElement('a')));
-        items[i].img.setAttribute('src', srcs[i]);
+        items.push(new Item(names[i], document.createElement('div'), document.createElement('img'), 
+        document.createElement('a')));
+
+        items[i].div.setAttribute('class','menuitem');
         items[i].img.setAttribute('class', 'menuimg');
-                items[i].img.setAttribute('id', i);
-        items[i].a.setAttribute('class','menuitem');
+        items[i].img.setAttribute('src', srcs[i]);
+        items[i].div.setAttribute('id', i);
+        items[i].a.setAttribute('id', i);
         items[i].a.textContent = items[i].name;   
     }
 }
 function addImgs(){
     for(i=names.length-1; i>=0; i--){
-        menu.appendChild(items[i].a);
-        items[i].a.appendChild(items[i].img);
+        menu.appendChild(items[i].div);
+        items[i].div.appendChild(items[i].a);
+        items[i].div.appendChild(items[i].img);
     }
 }
 function mouse(){
     $('.menuitem').hover(function(e){
-        if (e.target.className == 'menuitem'){
-        }
+        items[parseInt(e.target.id) + "<br>"].a.style.visibility = 'none';
+        console.log(e.target.id);
     }, function(e){
 
     });
